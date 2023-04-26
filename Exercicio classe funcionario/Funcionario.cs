56b4bf47d979697d2d56bb2575d63d2afaa643ea
vitor <vitor@DESKTOP-4OIHHQ9>
2023-04-26 16:08:28 -0300

    Add project files.

---
+++ Exercicio classe funcionario/Funcionario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_classe_funcionario
+{
+    internal class Funcionario
+    {
+
+        public string Nome;
+        public double Salario;
+
+    }
+}
---
+++ Exercicio classe funcionario/Program.cs
@@ -0,0 +1,23 @@
+﻿using Exercicio_classe_funcionario;
+using System.Globalization;
+
+
+Funcionario func1, func2;
+
+
+func1 = new Funcionario();
+func2 = new Funcionario();
+
+Console.Write("Informe aqui os dados do primeiro funcionario:  \r\nNome: ");
+func1.Nome = Console.ReadLine();
+Console.Write("Salario: ");
+func1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Informe aqui os dados do segundo funcionario:  \r\nNome: ");
+func2.Nome = Console.ReadLine();
+Console.Write("Salario: ");
+func2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double media = ((func2.Salario + func1.Salario) / 2);
+
+Console.WriteLine("A média dos salarios é: " + media.ToString("F2", CultureInfo.InvariantCulture));
